e1d3dc28d4b6315b054ca4f24869564e9138deaa
developer-tvc <developer@techversantinfo.com>
2020-12-30 12:40:53 +0530

    Enable authenication #8

--- backend/mahvie/api_v1/models.py
+++ backend/mahvie/api_v1/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+# from django.db import models
+# from django.utils.translation import gettext as _
 
-# Create your models here.
+# Female = 'F'
+# Male = 'M'
+# GENDER = [
+#     (Male, 'Male'),
+#     (Female, 'Female')
+# ]
+
+
+# class TimeStampedModel(models.Model):
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     class Meta:
+#         abstract = True
+
+
+# class Questionnaire(TimeStampedModel):
+#     name = models.CharField(max_length=255)
+#     questions = models.ManyToManyField(Question)
+
+
+# class Question(TimeStampedModel):
+#     question_text = models.CharField(max_length=255)
+#     question_category = models.CharField(max_length=2)
+
+
+# class UserResponse(TimeStampedModel):
+#     user_questionnaire = models.ForeignKey(
+#         Questionnaire, null=True, on_delete=models.SET_NULL)
+#     question = models.ForeignKey(
+#         Question, null=True, on_delete=models.SET_NULL)
+#     response_text = models.CharField(max_length=255)
