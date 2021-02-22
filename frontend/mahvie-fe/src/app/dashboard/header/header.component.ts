058314c39f12a1d5f54cf42410bb37633a741055
Ajosh <ajoshdl13@gmail.com>
2021-02-22 11:48:47 +0530

    commit

--- frontend/mahvie-fe/src/app/dashboard/header/header.component.ts
+++ frontend/mahvie-fe/src/app/dashboard/header/header.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { QuestionnaireService } from '@services/questionnaire.service';
 
@@ -36,42 +35,3 @@ export class HeaderComponent implements OnInit {
     }
   }
 }
-=======
-import { Component, OnInit } from '@angular/core';
-import { QuestionnaireService } from '@services/questionnaire.service';
-
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-})
-export class HeaderComponent implements OnInit {
-  // isButtonVisible = Number(localStorage.getItem('questionId')) > 1;
-
-  constructor(
-    private questionService: QuestionnaireService,
-  ) { }
-
-  ngOnInit(): void {
-    // this.questionService.backButtonVisibility.subscribe(_ => {
-    //   this.isButtonVisible = Number(localStorage.getItem('questionId')) > 1;
-    // });
-  }
-
-  /**
-   * Emits event to show previous question
-   */
-  previous(): void {
-    this.questionService.backButtonClick.next();
-  }
-
-  menuFunction(){
-    let x:any = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-}
->>>>>>> frontend
